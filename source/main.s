27421c275cf994c4c20f79376c128a6f5ab14ea3
Semy Levy <semylevy@hotmail.com>
2018-05-01 20:41:54 -0500

    Screen 04

--- source/main.s
+++ source/main.s
@@ -34,15 +34,37 @@ main:
     noError$:
         bl SetGraphicsAddress
 
-    mov r0,#9
-    bl FindTag
-    ldr r1,[r0]
-    lsl r1,#2
-    sub r1,#8
-    add r0,#8
-    mov r2,#0
-    mov r3,#0
-    bl DrawString
-
+    mov r4,#0
     loop$:
+    ldr r0,=format
+    mov r1,#formatEnd-format
+    ldr r2,=formatEnd
+    lsr r3,r4,#4
+    push {r3}
+    push {r3}
+    push {r3}
+    push {r3}
+    bl FormatString
+    add sp,#16
+
+    mov r1,r0
+    ldr r0,=formatEnd
+    mov r2,#0
+    mov r3,r4
+
+    add r3,#10
+    cmp r3,#768-16
+    subhi r3,#664
+    addhi r2,#20
+    bl DrawString
+    ldr r0,=100000
+    bl TimerFunction
+
+    add r4,#16
     b loop$
+
+    .section .data
+    format:
+    .ascii "HOLA KAREN"
+    formatEnd:
+
